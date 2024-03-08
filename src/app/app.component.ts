fed54ec4407720946d2cf70548e3701c04749903
Italo Mangueira <italomangueira14@gmail.com>
2024-03-08 16:18:19 -0300

    Add Category Section

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,9 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavBarComponent } from "./src/components/nav-bar/nav-bar.component";
 import { HeroComponent } from "./src/components/hero/hero.component";
+import { SlickCarouselModule } from 'ngx-slick-carousel';
+import { CategoryComponent } from "./src/components/category/category.component";
+
 
 
 @Component({
@@ -9,8 +12,8 @@ import { HeroComponent } from "./src/components/hero/hero.component";
     standalone: true,
     templateUrl: './app.component.html',
     styleUrl: './app.component.css',
-    imports: [RouterOutlet, NavBarComponent, HeroComponent]
+    imports: [RouterOutlet, NavBarComponent, HeroComponent, SlickCarouselModule, CategoryComponent]
 })
 export class AppComponent {
-  title = 'Nike';
+  title = 'ETRIX';
 }
--- src/app/src/components/hero/hero.component.ts
+++ src/app/src/components/hero/hero.component.ts
@@ -1,12 +1,55 @@
 import { Component } from '@angular/core';
+import { SlickCarouselModule } from 'ngx-slick-carousel';
+import { NgOptimizedImage } from '@angular/common';
 
 @Component({
   selector: 'app-hero',
   standalone: true,
-  imports: [],
   templateUrl: './hero.component.html',
-  styleUrl: './hero.component.css'
+  styleUrl: './hero.component.css',
+  imports: [SlickCarouselModule, NgOptimizedImage,]
 })
+
+
 export class HeroComponent {
+  slideConfig = {
+    "slidesToScroll": 1,
+    "dots": false,
+    "arrows": false,
+    "autoplay": true,
+    "autoplaySpeed": 5000,
+    "pauseOnHover": false,
+    "pauseOnFocus": true,
+    "infinite": true,
+    "cssEase": "ease-in-out"
+  };
+
+
+
+
+
+  HeroData = [
+    {
+      id: 1,
+      subtitle: "Beats Solo",
+      img: "https://github.com/dilshad-ahmed/Eshop-TCJ/blob/main/src/assets/hero/headphone.png?raw=true",
+      title: "Wireless",
+      title2: "Headphone",
+    },
+    {
+      id: 2,
+      subtitle: "Beats Solo",
+      img: "https://github.com/dilshad-ahmed/Eshop-TCJ/blob/main/src/assets/hero/watch.png?raw=true",
+      title: "Wireless",
+      title2: "Virtual",
+    },
+    {
+      id: 3,
+      img: "https://github.com/dilshad-ahmed/Eshop-TCJ/blob/main/src/assets/category/macbook.png?raw=true",
+      subtitle: "Beats Solo",
+      title: "Branded",
+      title2: "Laptops",
+    },
+  ];
 
 }
